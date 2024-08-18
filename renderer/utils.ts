62657a676844ab6d08315c0c3d4a5915f94e13f0
Ivan Uzun <ivan@motorway.co.uk>
2024-08-19 00:31:28 +0100

    chore: clean up

--- renderer/utils.ts
+++ renderer/utils.ts
@@ -1,7 +1,7 @@
 // Same as Object.assign() but with type inference
-export const objectAssign = <Obj extends object, ObjAddendum>(
+export function objectAssign<Obj extends object, ObjAddendum>(
   obj: Obj,
   objAddendum: ObjAddendum
-): asserts obj is Obj & ObjAddendum => {
+): asserts obj is Obj & ObjAddendum {
   Object.assign(obj, objAddendum)
 }
